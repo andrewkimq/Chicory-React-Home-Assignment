fd26b282034d2fed1441e56aa69e32f52b81df67
andrewkimq <andrewkimq@gmail.com>
2022-07-26 10:40:49 -0700

    Wrote inital logic for fetch custom hook

--- src/utils/customhooks/useFetchRetailers.tsx
+++ src/utils/customhooks/useFetchRetailers.tsx
@@ -1,3 +1,28 @@
-export default function useFetchRetailers() {
-    
+import { useState, useEffect } from "react";
+
+export default function useFetchRetailers(url: URL) {
+  const [retailerResponse, setRetailerResponse] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect((): void => {
+    const callFetch = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(url);
+        const newRetailerResponse = await response.json();
+        setRetailerResponse(newRetailerResponse);
+      } catch (error) {
+        let message = "Unknown Error";
+        if (error instanceof Error) {
+          message = error.message;
+        }
+        reportError({ message });
+        setRetailerResponse(null);
+      }
+      setIsLoading(false);
+    };
+    callFetch();
+  }, [url]);
+
+  return [retailerResponse, isLoading];
 }
