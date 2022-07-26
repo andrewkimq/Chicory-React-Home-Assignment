c94a01a2dcae7abd13ee047c0253d7909b883fd6
andrewkimq <andrewkimq@gmail.com>
2022-07-26 13:22:25 -0700

    Created Dropdown options component and moved data

---
+++ src/components/ChicoryDropdown/ChicoryDropdownOptions.tsx
@@ -0,0 +1,16 @@
+type DropdownProps = {
+  dropdowndata: {
+    id: string;
+    logoUrl: string;
+    name: string;
+    requiresLocation: boolean;
+    shopOnLogoUrl: string;
+    slug: string;
+  };
+};
+
+export default function ChicoryDropdownOptions({
+  dropdowndata,
+}: DropdownProps) {
+  return <div></div>;
+}
